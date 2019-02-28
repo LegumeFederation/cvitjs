543abff10ee61203531f82e33e56f6e1181628a6
awilkey <awilkey@iastate.edu>
2019-02-28 12:29:45 -0600

    Add remaining position glyphs

--- src/canvas/glyph/position/index.js
+++ src/canvas/glyph/position/index.js
@@ -1,9 +1,15 @@
 import Rect from './Rect';
+import Doublecircle from './Doublecircle';
+import Circle from './Circle';
 
 export default function Position(data,config,view,subglyph){
   switch (subglyph) {
     case 'rect':
       return new Rect(data,config,view);
+    case 'doublecircle':
+      return new Doublecircle(data,config,view);
+    case 'circle':
+      return new Circle(data,config,view);
 
     default:
       console.log('g-s not supp yet', glyph, subglyph);
