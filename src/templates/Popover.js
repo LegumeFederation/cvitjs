ff412fa19aa1168b8b8d24b6b568deef532835c5
awilkey <awilkey@iastate.edu>
2019-03-13 13:51:27 -0500

    Tweaks to popover

--- src/templates/Popover.js
+++ src/templates/Popover.js
@@ -1,43 +1,58 @@
 import {h} from 'preact';
 
-export function popoverContents(data){
-  return data.map((feature,i) =>{
+export function popoverContents(data) {
+
+  let attributeContents = data.map((feature, i) => {
     let attributes = [];
-    for( let key in feature.attribute){
-      if(feature.attribute.hasOwnProperty(key)){
-        attributes.push((<tr><th>{key}</th><th>{feature.attribute[key]}</th></tr>))
+    for (let key in feature.attribute) {
+      if (feature.attribute.hasOwnProperty(key)) {
+        attributes.push((<tr>
+          <th>{key}</th>
+          <th>{feature.attribute[key]}</th>
+        </tr>))
       }
     }
-    return(
+    return (
       <div className={'popover-contents'}>
         <table>
           <thead>
-            <tr>
-              <th> Name: </th>
-              <th> {feature.attribute.id || i} </th>
-            </tr>
+          <tr>
+            <th> Name:</th>
+            <th> {feature.attribute.id || i} </th>
+          </tr>
           </thead>
           <tbody>
-            <tr>
-              <th> Start: </th>
-              <th> {feature.start} </th>
-            </tr>
-            <tr>
-              <th> End: </th>
-              <th> {feature.end} </th>
-            </tr>
-            <tr>
-              <th> Strand: </th>
-              <th> {feature.strand} </th>
-            </tr>
-            <tr>
-              <th> Score: </th>
-              <th> {feature.strand} </th>
-            </tr>
-            {attributes}
+          <tr>
+            <th> Start:</th>
+            <th> {feature.start} </th>
+          </tr>
+          <tr>
+            <th> End:</th>
+            <th> {feature.end} </th>
+          </tr>
+          <tr>
+            <th> Strand:</th>
+            <th> {feature.strand} </th>
+          </tr>
+          <tr>
+            <th> Score:</th>
+            <th> {feature.strand} </th>
+          </tr>
+          {attributes}
           </tbody>
         </table>
       </div>
     )
-  })
+  });
+
+  return(
+    <div id={'popover-contents'}>
+      <h5> Selection </h5>
+      <hr/>
+      <div className={'popover-details'}>
+        {attributeContents}
+      </div>
+    </div>
+  )
 }
+
