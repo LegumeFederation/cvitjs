ae84d77222b9a863e53ee730b3190ff3c13b15c1
Annie <abrown1@iastate.edu>
2020-01-09 10:28:47 -0600

    Update About.js

--- ui/src/Components/HelpTopics/About.js
+++ ui/src/Components/HelpTopics/About.js
@@ -9,7 +9,7 @@ export default class About extends React.Component {
                 </div>
                 <div className={'pure-g'}>
                     <div className={'pure-u-1-1 l-box modal-section'}>
-                        <p> GCViT: a multiple chromosome visualisation tool for the comparison of multiple accessions from a single dataset. </p>
+                        <p> GCViT: a multiple chromosome visualisation tool for visualizing resequencing or SNP array data. GCViT allows a user to compare multiple accessions to a user selected reference accession from a single dataset and visually identify regions of similarity and difference across the genome. </p>
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/B2gPVUipWo0" frameBorder="0"
                                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen title="Introduction to GCViT">
