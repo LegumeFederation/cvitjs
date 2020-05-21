badea3f1cc5b57da6d3f81f5bad5d997a9c594bf
awilkey <awilkey@iastate.edu>
2020-05-21 12:28:00 -0500

    finding out which box breaks rendering

--- src/ui/body/modal/ColorSelect.js
+++ src/ui/body/modal/ColorSelect.js
@@ -161,12 +161,12 @@ export default class ColorModal extends Component {
     aSlide.position.y = aGra.bounds.topLeft.y;
 
     /** set pointer position and box colors */
-    this.setPosition(pointer,pGra,sSlide,sGra,aSlide,aGra,this.props.cColors[this.props.target]);
-    this.changeColor(pointer,pGra,sSlide,sGra,aSlide,aGra,sRad,colPrev);
+    //this.setPosition(pointer,pGra,sSlide,sGra,aSlide,aGra,this.props.cColors[this.props.target]);
+    //this.changeColor(pointer,pGra,sSlide,sGra,aSlide,aGra,sRad,colPrev);
     //this.changeColor(pointer,pGra,sSlide,sGra,aSlide,aGra,sRad,colPrev);
 
     //paper.view.draw();
-    let s = this.state;
+  //  let s = this.state;
 
     pGra.onMouseDown = (e) => {
       pointer.position = e.point;
