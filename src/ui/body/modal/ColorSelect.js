caae5621b6afb24483c8bac6f6d27413f1eefc30
awilkey <awilkey@iastate.edu>
2020-05-21 11:29:16 -0500

    finding out which box breaks rendering

--- src/ui/body/modal/ColorSelect.js
+++ src/ui/body/modal/ColorSelect.js
@@ -219,11 +219,11 @@ export default class ColorModal extends Component {
     pointer.position.y = pGra.bounds.height*-color.brightness + pGra.bounds.height + pGra.bounds.topLeft.y;
     sSlide.position.y = sGra.bounds.height*-color.saturation + sGra.bounds.height + sGra.bounds.topLeft.y;
     aSlide.position.y = aGra.bounds.height*-color.alpha + aGra.bounds.height + aGra.bounds.topLeft.y;
-  //  this.setState(
+    this.setState(
   //    pointer: pointer,
-  //    sSlide: sSlide,
-  //    aSlide: aSlide
-  //  });
+      sSlide: sSlide,
+      aSlide: aSlide
+    });
   }
 
   changeColor (pointer,pGra,sSlide,sGra,aSlide,aGra,sRad,colPrev){
