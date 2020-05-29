16692e2b48a33a09c069d719120259588ee7efd5
awilkey <awilkey@iastate.edu>
2020-05-29 15:56:08 -0500

    Update default build to include /demo route

--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c0e37228a6017dca3a212f949a8ea68.js"
+  "/precache-manifest.f765fa4cb250a818a3e618b5fa857bb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
