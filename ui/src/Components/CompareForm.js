e1dfd2bd1ed3d4f300365b43b2e22b3dc3922469
awilkey <awilkey@iastate.edu>
2020-06-03 18:01:17 -0500

    add comments

--- ui/src/Components/CompareForm.js
+++ ui/src/Components/CompareForm.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import GenotypeSelector from './GenotypeSelect';
-
 /**
- * Select comparison genotypes
+ * React component for rendering the comparison genotype selection menu
  */
+
+import React from 'react';
+import GenotypeSelector from './GenotypeSelect'; //Generic gt select component
+
 export default class CompareForm extends React.Component {
 
 	state = {
-		options: [],
-		count: 1
+		options: [], // currently selected comparisons
+		count: 1 // tracks actual length of non-null entries.
 	};
-
+	// remove option selection
 	removeOption = (rm) => {
 		let options = this.state.options.filter(opt => {
 			return opt.key !== `${rm}`;
@@ -18,14 +19,14 @@ export default class CompareForm extends React.Component {
 		this.props.appendDataset(rm,null);
 		this.setState({options});
 	}
-
+        // add option selection
 	addOption = () => {
 		let options = this.state.options.concat([
 			<GenotypeSelector
 				key={this.state.count}
 				idx={this.state.count}
 				selected={this.props.selected}
-				genotypes={this.props.genotypes}A
+				genotypes={this.props.genotypes}
 				appendDataset={this.props.appendDataset}
 				removeOption={this.removeOption}
 			/>
@@ -33,12 +34,14 @@ export default class CompareForm extends React.Component {
 		this.setState({options,count:this.state.count+1});
 	};
 
+	// prevents issues when all options are removed
 	componentDidUpdate(prevProps, prevState, snapshot) {
 		if(this.props.selected !== prevProps.selected){
 			this.setState({'count':1, 'options':[]})
 		}
 	}
 
+	// render component
 	render() {
 		return (
 			<fieldset className={'genotype-field'}>
