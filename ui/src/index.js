4bc53a933204e6883c9203172e0a146817f1d616
awilkey <awilkey@iastate.edu>
2020-06-03 11:00:51 -0500

    Condense css to one file

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import 'purecss/build/grids.css';
 import 'purecss/build/buttons.css';
 import './index.css';
-import './selector.css';
-import './style.css';
 import App from './App';
 
 // Initial entry point, bind gcvit ui to 'selector-app' div
